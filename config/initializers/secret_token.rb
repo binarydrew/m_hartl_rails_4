c0c15130b6bab735474d8873c34cac7f25a30965
YO <aftl84@gmail.com>
2013-07-26 14:13:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+MHartlSampleApp::Application.config.secret_key_base = '6325d5bb72cd8c12203130673d81036f65294befd9b4e65e34bc193b56cab9cf28f766e4d910f2652f2b7b0f6ca484142e1635929f7a15e59ae3b2750bc766ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MHartlSampleApp::Application.config.session_store :cookie_store, key: '_m_hartl_sample_app_session'
